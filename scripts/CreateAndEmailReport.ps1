740f7189047fe368aeccb242424ec1943ce06b22
Scott Glew <1786359+scottglew@users.noreply.github.com>
2024-10-18 14:32:48 +0800

    Added link to video in the script's description

--- scripts/CreateAndEmailReport.ps1
+++ scripts/CreateAndEmailReport.ps1
@@ -1,6 +1,7 @@
 # This script generates a report for a specified time range in the day, 
 # filtered by a list of users and sends an email with the private link to the report, 
 # as well as PDF and CSV attachments.
+# Here's a video on how to use it: https://www.loom.com/share/21cfc712542d434d803c0034f6accad3?sid=0937bcde-9ce0-48a0-9669-45a66601cef7
 
 # Define API Base URL
 $apiBaseUrl = "http://your_fastvue_site/_/api?f="
